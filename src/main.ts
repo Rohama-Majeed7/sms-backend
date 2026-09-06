57713418bb5d7fe69a202ee646a956f59537dc63
Rohama Majeed <rohamamajeed3@gmail.com>
2026-09-06 09:55:26 +0300

    origin url set

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   app.enableCors({
-    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    origin: ['https://sms-user-portal.vercel.app', 'http://localhost:5173'],
     credentials: true,
   });
 
